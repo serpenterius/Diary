dbfe061872931bee87211212be402a655cf76564
Shiro <nellishiro@gmail.com>
2025-05-21 22:23:47 +0300

    A little work on habit tracker

--- src/HabitsTracker/HabitsList.jsx
+++ src/HabitsTracker/HabitsList.jsx
@@ -22,10 +22,18 @@ function HabitsList(props) {
         //         </div>
         //     </li>
         // </ul>
-        <ul>
+        <ul className="w-full px-4">
             {habitsList.map(item => (
-                <li key={item.id}>
+                <li key={item.id}
+                    className="w-full flex items-center justify-between border-2 border-black my-5 p-3 text-violet-600">
                     {item.text}
+                    <div className="flex justify-between border-2 border-black w-48">
+                        <p>Streak : 5</p>
+                        {/* <input type="checkbox"
+                                className="appearance-none w-8 h-8 rounded-2xl border-2 border-violet-600
+                                            checked:bg-violet-600 checked:"/> */}
+                        <button className="">Выполнить</button>
+                    </div>
                 </li>
             ))}
         </ul>
