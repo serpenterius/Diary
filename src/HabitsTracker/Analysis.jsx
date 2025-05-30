50d698789b15af02f6575a189d80e0aebdc99cad
Shiro <nellishiro@gmail.com>
2025-05-30 19:34:00 +0300

    Little changes

--- src/HabitsTracker/Analysis.jsx
+++ src/HabitsTracker/Analysis.jsx
@@ -1,13 +1,16 @@
-
+import { useState } from "react"
 
 function Analysis(props) {
+    const [isVisible, setIsVisible] = useState(false)
     const visible = 'border-2 border-black w-full'
     const hidden = 'border-2 border-black w-full hidden'
 
+
     return (
-        <div className="w-full flex flex-col">
-            <button className="">Аналитика</button>
-            <div className="border-2 border-black w-full">
+        <div className="w-full flex flex-col items-center">
+            <button onClick={() => setIsVisible(!isVisible)}
+                    className="border-2 border-violet-600 rounded-full px-3 py-2 my-3 text-violet-600">Аналитика</button>
+            <div className={isVisible ? 'border-2 border-black w-5/6 rounded-3xl p-2' : 'border-2 border-black w-5/6 rounded-3xl hidden'}>
                 <ul>
                     {props.habits.map(item => (
                         <li className="border-2 border-black">{item.text}</li>
